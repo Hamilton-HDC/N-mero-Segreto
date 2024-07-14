7c9380d5383161e55a462ceea9dee5a8cfc0475f
Hamilton Cesar <Hamiltondc.dc@gmail.com>
2024-07-14 02:24:29 -0300

    continuando a Lógica

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 4;
-let chute = prompt('Escolha um número entre 1 à 30')
-console.log(numeroSecreto)
+let numeroSecreto = 25;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 
-// Verificar se o chute é igual ao numeroSecreto
-if (numeroSecreto == chute) {
-   alert('Isso ai! Você descobriu o número secreto que era: ' + numeroSecreto)
+// enquanto (loop) alguma coisa não acontecer o programa não para
+while (chute != numeroSecreto) {
+   chute = prompt('Escolha um número entre 1 à 30');
 
+   // Verificar se o chute é igual ao numeroSecreto
+   if (numeroSecreto == chute) {
+      break
+
+   } else {
+      if (chute < numeroSecreto) {
+         alert(`O número secreto é maior que ${chute}`);
+      } else {
+         alert(`O número secreto é menor que ${chute} `);
+      }
+      //tentativas = tentativas + 1;
+      tentativas++;
+
+   }
+}
+if (tentativas > 1){
+   alert(`Isso ai! Você descobriu o número secreto que era:  ${numeroSecreto} com ${tentativas} tentativas`)
 } else {
 
-   alert('Você errou! tente novamente :(')
+   alert(`Isso ai! Você descobriu o número secreto que era:  ${numeroSecreto} com ${tentativas} tentativa`)
 }
