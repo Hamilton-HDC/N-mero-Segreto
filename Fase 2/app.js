5a2a429d48ef5adee0b586ac0dbb02ee92408e67
Hamilton Cesar <Hamiltondc.dc@gmail.com>
2024-07-16 07:22:15 -0300

    Logica do botão chute criada

--- Fase 2/app.js
+++ Fase 2/app.js
@@ -1,19 +1,40 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo do número secreto';
 
 // let paragrafo = document.querySelector('p')
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function exibirTextoNaTela(tag,texto){
-   let campo = document.querySelector(tag);
-   campo.innerHTML = texto;
-   
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+verificarChute();
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior");
+    }
+    tentativas++;
+  }
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
 
- function verificarChute(){
-   console.log('O botão foi clicado');
- }
